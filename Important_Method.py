b7d72bd155769ae1de797abebad4da1b5993ba47
uppaltushar <40592029+uppaltushar@users.noreply.github.com>
2018-08-20 18:38:20 +0530

    Add files via upload

---
+++ Important_Method.py
@@ -0,0 +1,49 @@
+
+#Question 1
+LIST=[1,2,3,4,5]
+print(list(reversed(LIST)))
+
+#Question 2
+str=input("Enter A string")
+for i in range(len(str)):
+    if str[i].isupper():
+        print(str[i])
+
+#Question 3
+a=input("Enter").split(",")
+list=[]
+for i in a:
+    list.append(int(i))#typecast into integer
+print(list)
+
+#Question 4
+n=int(input("Enter-number"))
+b=n
+rem=0
+result=0
+while n!=0:
+    rem=n%10
+    result=result*10+rem
+    n//=10
+if b==result:
+    print("It is aPalandromic Number")
+else:
+    print("It is Not a Palandromic Number")
+
+#Question 5
+from copy import *
+print("DeepCopy:- ")
+list1 = ['a','b',['ab','ba']]
+
+list2 = deepcopy(lst1)
+
+list2[2][1] = "d"
+list2[0] = "c";
+
+print(list2)
+print(list1)
+print("Shallow Copy:- ")
+list3=list1
+list3[0]="hi"
+print(list3)
+print(list1)
